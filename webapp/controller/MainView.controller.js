d05fe28dfe6a51c5fe95d898c243c30281ae4e83
dante tagliavini <dftagliavini@indracompany.com>
2025-04-15 14:37:27 +0000

    cambios

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("sydenoa.sydenoa.controller.MainView", {
-        onInit() {
+        onInit: function() {
 
             const oJSONModel = new sap.ui.model.json.JSONModel();
             const oView = this.getView();
@@ -12,6 +14,28 @@ sap.ui.define([
             oView.setModel(oJSONModel,"combo");
 
 
+        },
+        onFilter : function(event){
+
+        const Odata = this.getView().getModel("combo").getData();
+        let filters = [];
+        if (Odata.ShipName !== ""){
+            filters.push(new Filter("ShipName",FilterOperator.Contains,Odata.ShipName));
+        }
+         
+        if (Odata.ControlKey !== ""){
+            filters.push(new Filter("Country",FilterOperator.EQ,Odata.ControlKey));
+        }
+
+        const list = this.getView().byId("ListadoInvoices");
+        const oBinding = list.getBinding("items");
+        oBinding.filter(filters);
+
+        },
+        onClearFilter:function(event){
+            const oView = this.getView().getModel("combo");
+            oView.setProperty("/ShipName","");
+            oView.setProperty("/CountryKey","");
         }
     });
 });
